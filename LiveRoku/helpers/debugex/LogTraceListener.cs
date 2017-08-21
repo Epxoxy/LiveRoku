7a59cdc07615e4db9fe420275cb2f8a14b697fa8
Epxoxy <xiaoxiaodx@outlook.com>
2017-08-21 13:37:50 +0800

    add project files

---
+++ LiveRoku/helpers/debugex/LogTraceListener.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LiveRoku
+{
+    internal class LogTraceListener : System.Diagnostics.TraceListener {
+        private StringBuilder debugMsgBuilder = new StringBuilder ();
+        private string path;
+        private int maxSize;
+
+        public LogTraceListener (string path, int maxSize) {
+            this.path = path;
+            this.maxSize = maxSize;
+        }
+
+        public override void Write (string message) {
+            debugMsgBuilder.Append (message);
+            if (debugMsgBuilder.Length > maxSize) {
+                WriteToFileAndClear ();
+            }
+        }
+
+        public void WriteToFileAndClear () {
+            if (debugMsgBuilder.Length <= 0) return;
+            try {
+                Storage.FileHelper.writeTxt (debugMsgBuilder.ToString (), path, true);
+            } catch (Exception e) { }
+            debugMsgBuilder.Clear ();
+        }
+
+        public override void WriteLine (string message) {
+            if(!string.IsNullOrEmpty(message))
+                message = message.Replace ("\n", "\n----------------");
+            Write ("[" + DateTime.Now.ToString ("HH:mm:ss.fff") + "] " + message + Environment.NewLine);
+        }
+    }
+}
